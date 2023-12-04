ef8b721ad0bdd81cb6265ee61185875fef23fce2
Davi Macedo Mendes <macedomendesdavi@gmail.com>
2023-12-04 00:49:08 -0300

    Alteracoes dia 03/10

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,24 @@ class Upgrade {
 
   double clickRateMultiplier;
   double clickRateBase;
+  double autoClickIndvValue;
   double clickIndvValue;
+  int upgradeLevel;
 
   double manualClickMultiplier;
+
+  String iconPath;
 
   Upgrade({
     required this.name,
     required this.baseCost,
     required this.clickRateMultiplier,
+    required this.autoClickIndvValue,
     required this.clickIndvValue,
+    required this.upgradeLevel,
     required this.manualClickMultiplier,
     required this.clickRateBase,
+    required this.iconPath,
   });
 }
 
@@ -46,8 +53,9 @@ class _BitcoinClickerState extends State<BitcoinClicker> {
   int clickUpgradeLevel = 0;
   int manualClickUpgradeLevel = 0;
   List<Upgrade> upgrades = [
-    Upgrade(name: 'Valor do Click', baseCost: 10, clickRateMultiplier: 0, clickIndvValue: 0, manualClickMultiplier: 0.3, clickRateBase: 0),
-    Upgrade(name: 'Placa de Vídeo', baseCost: 40.0, clickRateMultiplier: 0.5, clickIndvValue: 0, manualClickMultiplier: 0, clickRateBase: 1),
+    Upgrade(name: 'Valor do Click', baseCost: 10, clickRateMultiplier: 0, autoClickIndvValue: 0, clickIndvValue: 0, upgradeLevel: 0, manualClickMultiplier: 0.3, clickRateBase: 0, iconPath: '../assets/upgradesIcons/mouse.png'),
+    Upgrade(name: 'MTX 340', baseCost: 40.0, clickRateMultiplier: 0.5, autoClickIndvValue: 0, clickIndvValue: 0, upgradeLevel: 0, manualClickMultiplier: 0, clickRateBase: 1, iconPath: '../assets/upgradesIcons/placa_video_upgrade_1.png'),
+    Upgrade(name: 'MTX 720', baseCost: 300, clickRateMultiplier: 2.7, autoClickIndvValue: 0, clickIndvValue: 0, upgradeLevel: 0, manualClickMultiplier: 5, clickRateBase: 10, iconPath: '../assets/upgradesIcons/placa_video_upgrade_2.png')
     // Adicione mais upgrades conforme necessário
   ];
 
@@ -68,20 +76,24 @@ class _BitcoinClickerState extends State<BitcoinClicker> {
       setState(() {
         bitcoins -= upgrade.baseCost;
 
-        if (upgrade.clickRateBase == 0) {
+        if (upgrade.upgradeLevel == 0) {
           clickPerSecond += upgrade.clickRateBase;
-          upgrade.clickIndvValue += upgrade.clickRateBase;
+          upgrade.autoClickIndvValue += upgrade.clickRateBase;
         } else {
           clickPerSecond += upgrade.clickRateBase * upgrade.clickRateMultiplier;
-          upgrade.clickIndvValue += upgrade.clickRateBase * upgrade.clickRateMultiplier;
+          upgrade.autoClickIndvValue += upgrade.clickRateBase * upgrade.clickRateMultiplier;
         }
 
         clickValue += upgrade.manualClickMultiplier;
+        upgrade.clickIndvValue += upgrade.manualClickMultiplier;
+        upgrade.upgradeLevel++;
 
-        if (upgrade.name == 'Placa de Vídeo') {
-          upgrade.baseCost *= 1.3;
-        } else if (upgrade.name == 'Valor do Click') {
+        if (upgrade.name == 'Valor do Click') {
           upgrade.baseCost *= 1.22;
+        } else if (upgrade.name == 'MTX 340') {
+          upgrade.baseCost *= 1.34;
+        } else if (upgrade.name == 'MTX 720') {
+          upgrade.baseCost *= 1.56;
         }
       });
 
@@ -106,15 +118,12 @@ class _BitcoinClickerState extends State<BitcoinClicker> {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Bitcoin Clicker'),
-      ),
       body: Stack(
         children: [
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/fundo.gif'), // Substitua pelo caminho correto do seu arquivo GIF
+                image: AssetImage('../assets/1.png'), // Substitua pelo caminho correto do seu arquivo GIF
                 fit: BoxFit.cover,
               ),
             ),
@@ -131,25 +140,24 @@ class _BitcoinClickerState extends State<BitcoinClicker> {
                 Container(
                   width: 150.0,
                   height: 150.0,
-                  child: Ink.image(
-                    image: AssetImage('assets/button.png'), // Substitua pelo caminho correto da sua imagem
-                    fit: BoxFit.cover,
+                  child: Material(
+                    color: Colors.transparent,
                     child: InkWell(
                       onTap: () => manualClick(),
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text(''),
+                      child: Image.asset(
+                        '../assets/moeda.gif',
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
                 ),
                 SizedBox(height: 20.0),
                 Text(
-                  'Bitcoins por segundo: ${clickPerSecond.toStringAsFixed(2)}',
+                  'Bitcoins por segundo: ${(clickPerSecond/10000).toStringAsFixed(4)}',
                   style: TextStyle(fontSize: 16.0, color: Colors.white),
                 ),
                 Text(
-                  'Valor do Clique: $clickValue',
+                  'Valor do Clique: ${(clickValue/10000).toStringAsFixed(4)}',
                   style: TextStyle(fontSize: 16.0, color: Colors.white),
                 ),
                 SizedBox(height: 10.0),
@@ -163,62 +171,85 @@ class _BitcoinClickerState extends State<BitcoinClicker> {
               ],
             ),
           ),
-          if (showUpgrades)
-            AnimatedPositioned(
-              duration: Duration(milliseconds: 300),
-              curve: Curves.easeInOut,
-              top: 0,
-              bottom: 0,
-              right: 0,
-              child: Container(
-                width: 400,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(38, 44, 58, 0.898)
-                ),
-                child: ListView(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    ListTile(
-                      title: Text(
-                        'Loja de Upgrades',
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                    ),
-                    for (var upgrade in upgrades)
-                      Container(
-                        padding: EdgeInsets.fromLTRB(0, 0, 0, 70),
-                        margin: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/backgroundupgrade.png'),
-                            fit: BoxFit.fill
-                          ),
-                        ),
-                        child: Tooltip(
-                          message: 'Bitcoins por segundo: +${upgrade.clickIndvValue.toStringAsFixed(1)}\n'
-                              'Aumento por segundo: +${(upgrade.clickRateMultiplier * 100).toStringAsFixed(0)}%\n'
-                              'Aumento para cliques manuais: +${(upgrade.manualClickMultiplier * 100).toStringAsFixed(0)}%',
-                          child: ListTile(
-                            contentPadding: EdgeInsets.only(left: 20),
-                            textColor: Colors.white,
-                            title: Text('${upgrade.name} - ${(upgrade.baseCost).toStringAsFixed(1)} bitcoins'),
-                          onTap: () => buyUpgrade(upgrade),
-                          ),
-                        ),
-                      ),
-                  ],
-                ),
+          AnimatedPositioned(
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+      top: 0,
+      bottom: 0,
+      right: showUpgrades ? 0 : -400,
+      child: Container(
+        width: 400,
+        padding: EdgeInsets.only(left: 20, top: 30),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('../assets/janelafundo.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            ListTile(
+              title: Text(
+                'Loja de Upgrades',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 37, 37, 37)),
               ),
             ),
-        ],
+            for (var upgrade in upgrades)
+              Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/backgroundupgrade.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                child: ListTile(
+                  leading: Image.asset(upgrade.iconPath, width: 100, height: 100),
+                  title: Text(
+                    '${upgrade.name} - ${((upgrade.baseCost) / 10000).toStringAsFixed(4)} bitcoins',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Bitcoins por segundo: +${(upgrade.autoClickIndvValue / 10000).toStringAsFixed(5)}',
+                        style: TextStyle(fontSize: 18, color: Color.fromRGBO(1, 255, 1, 1)),
+                      ),
+                      Text(
+                        'Bitcoins por click: +${(upgrade.clickIndvValue / 10000).toStringAsFixed(5)}',
+                        style: TextStyle(fontSize: 18, color: Color.fromRGBO(1, 175, 255, 1)),
+                      ),
+                      Text(
+                        'Aumento por segundo: +${((upgrade.clickRateMultiplier) / 10000).toStringAsFixed(5)}',
+                        style: TextStyle(fontSize: 16, color: Color.fromRGBO(199, 199, 199, 1)),
+                      ),
+                      Text(
+                        'Aumento para cliques: +${((upgrade.manualClickMultiplier) / 10000).toStringAsFixed(5)}',
+                        style: TextStyle(fontSize: 16, color: Color.fromRGBO(199, 199, 199, 1)),
+                      ),
+                    ],
+                  ),
+                  onTap: () => buyUpgrade(upgrade),
+                ),
+              ),
+          ],
+        ),
       ),
+    ),
+  ],
+    ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
             showUpgrades = !showUpgrades;
           });
         },
-        child: Icon(Icons.shopping_cart),
+        child: showUpgrades
+          ? Icon(Icons.close)
+          :Image.asset('../assets/23.png', width: 100, height: 100),
       ),
     );
   }
